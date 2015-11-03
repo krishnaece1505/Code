f29159fc3f80fb0491a684b9bf5c16bb4e8547c6
Kannan <krishnaece1505@gmail.com>
2015-11-04 00:56:43 +0100

    C code

---
+++ C/stack.c
@@ -0,0 +1,125 @@
+--------------------------------------------------------------------------------- 
+-- Module : STACK USING C
+--
+-- Author : Krishnaswamy Kannan
+---------------------------------------------------------------------------------
+
+#define _CRT_SECURE_NO_WARNINGS // To disable scanf error/warning in Visual studio
+#include <stdio.h>
+#include <stdlib.h> // For use of system() to clear the screen
+#define STACKSIZE 5 //define the size of the stack here
+
+/* Structure definition for stack */
+struct stack 
+{
+	int stk[STACKSIZE];
+	int top;
+	
+};
+
+typedef struct stack STACK;
+STACK s;
+
+void push(void);
+int pop(void);
+void display(void);
+
+void main()
+{
+	int choice;
+	char op = 'Y';
+	system("@cls||clear"); // Clear the console
+	s.top = -1;
+	printf(" ---------------- STACK MODEL ----------------\n");
+	while (op == 'Y' || op == 'y')
+	{
+		printf("\n STACK OPERATIONS\n");
+		printf("------------------------------------------\n");
+		printf(" 1 --> PUSH \n");
+		printf(" 2 --> POP \n");
+		printf(" 3 --> DISPLAY \n");
+		printf(" 4 --> EXIT \n");
+		printf("------------------------------------------\n");
+		
+		printf("Enter your choice\n");
+		scanf(" %d", &choice);
+
+		switch (choice)
+		{
+		case 1: push();
+			break;
+		case 2: pop();
+			break;
+		case 3: display();
+			break;
+		case 4: return;
+		default: printf("Select from the options above\n");
+			break;
+		}
+		
+		fflush(stdin);
+		printf("Do you want to continue(Type Y or N)? : ");
+		scanf(" %c", &op);
+	
+		system("@cls||clear");
+	}
+}
+
+/*Function to add an element to the stack*/
+void push()
+{
+	int num;
+	if (s.top == (STACKSIZE - 1))
+	{
+		printf("Stack is Full\n");
+		return;
+	}
+	else
+	{
+		printf("Enter the element to be pushed\n");
+		scanf(" %d", &num);
+		s.top = s.top + 1;
+		s.stk[s.top] = num;
+	}
+	return;
+}
+
+/*Function to delete an element from the stack*/
+int pop()
+{
+	int num;
+	if (s.top == -1)
+	{
+		printf("Stack is Empty\n");
+		return (s.top);
+	}
+	else
+	{
+		num = s.stk[s.top];
+		printf("The element %d is popped out of the stack\n", s.stk[s.top]);
+		s.top = s.top - 1;
+	}
+	return(num);
+}
+
+/*Function to display the status of the stack*/
+void display()
+{
+	int i;
+	if (s.top == -1)
+	{
+		printf("Stack is empty\n");
+		return;
+	}
+	else
+	{
+		printf("\n ------ STACK ------ \n\n");
+		for (i = s.top; i >= 0; i--)
+		{
+			//printf("-------\n");
+			printf("  %d", s.stk[i]);
+			printf("\n-------\n");
+		}
+	}
+	printf("\n");
+}
---
+++ C/string_search.c
@@ -0,0 +1,58 @@
+--------------------------------------------------------------------------------- 
+-- Module : WORD (OR) SUBSTRING SEARCH FROM A STRING
+--
+-- Author : Krishnaswamy Kannan
+---------------------------------------------------------------------------------
+#define _CRT_SECURE_NO_WARNINGS // To disable scanf error/warning in Visual studio
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h> // For use of system() to clear the screen
+
+void main()
+{
+	char str[500], search[30];
+	char op = 'Y';
+	system("@cls||clear"); // Clear the console
+	 
+	int count1 = 0, count2 = 0, i, j, flag;
+
+	while (op == 'Y' || op == 'y')
+	{
+	printf("\n ----------------- WORD (or) SUBSTRING SEARCH -----------------");
+	printf("\n\n  Enter a string: ");
+	scanf(" %[^\n]s", &str); // Escape sequence [^\n]s to support spaces in scanf
+
+	printf("\n  Enter the word (or) substring to search: ");
+	scanf(" %s", &search);
+
+	while (str[count1] != '\0')
+		count1++;
+
+	while (search[count2] != '\0')
+		count2++;
+
+	for (i = 0; i <= count1 - count2; i++)
+	{
+		for (j = i; j<i + count2; j++)
+		{
+			flag = 1;
+			if (str[j] != search[j - i])
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			break;
+	}
+	if (flag == 1)
+		printf("\n  SEARCH SUCCESSFUL!");
+	else
+		printf("\n  SEARCH UNSUCCESSFUL!");
+	printf("\n\n  Do you want to continue(Type Y or N)? : ");
+	scanf(" %c", &op);
+
+	system("@cls||clear");
+	}
+	
+}
